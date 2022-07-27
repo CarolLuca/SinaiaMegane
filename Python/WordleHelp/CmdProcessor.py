879f7ab5ed655c5158e18e1bcbc36575546f8c27
Carol Luca Gasan <gasancarolluca@gmail.com>
2022-07-27 11:37:35 +0300

    processAdd + update + check

--- Python/WordleHelp/CmdProcessor.py
+++ Python/WordleHelp/CmdProcessor.py
@@ -1,3 +1,6 @@
+import os
+import re
+
 ###
 # Class CmdProcessor is in charge of implementing all commands supported by WordleHelp
 from WordChecker import WordChecker
@@ -12,15 +15,45 @@ class CmdProcessor:
     # Class constructor
     def __init__(self):
         self._wordChecker = WordChecker()
+        self._dictionary = {}
 
     def processHelp(self):
         print("process command 'help'")
 
     def processAdd(self, args = None):
         print("Process command 'add'")
+        
+        if args == None:
+            raise Exception(f"Missing or invalid argument '{args}'. Expected file path!")
+        filePath = args
+        if not os.path.isfile(filePath):
+            raise Exception(f"File {filePath} cannot be located!")
+        
+        lines = open(filePath)
+        for line in lines:
+            pair = re.split("\t", line)
+            key = str(pair[0])
+            val = int(pair[1])
+            self._dictionary[key] = val
+        
+        # print(self._dictionary)
 
     def processMatch(self, args = None):
         print("Process command 'match'")
+
+        if args == None:
+            raise Exception(f"Missing or invalid argument '{args}'. Expected file path!")
+        hint = args
+
+        good_words = {}
+        self._wordChecker.update(hint)
+        for word in self._dictionary:
+            if self._wordChecker.check(word):
+                good_words[word] = self._dictionary[word]
+        
+        return good_words
+
+
     
     def processReset(self, args = None):
         print("Process command 'reset'")
@@ -36,3 +69,9 @@ class CmdProcessor:
 if __name__ == "__main__":
     cmdP = CmdProcessor()
     cmdP.processHelp()
+    cmdP.processAdd("Python\WordleHelp\words.txt")
+
+
+    # + verde
+    # - negru
+    # ~ galben
--- Python/WordleHelp/WordChecker.py
+++ Python/WordleHelp/WordChecker.py
@@ -1,3 +1,5 @@
+allLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
 ###
 # Class owning the set of hints given in the course
 # of playing one game of Wordle
@@ -8,15 +10,40 @@ class WordChecker:
     def __init__(self):
         print(f"TODO: WordChecker is initialized")
 
+        self._neededLetters = set({})
+        self._wrongLetters = [set({}),set({}),set({}),set({}),set({})]
+        self._goodLetters = ['','','','','']
+
     ###
     # Updates the internal state of the checker with a new hint
     def update(self, hint):
         print(f"TODO: WordChecker adding the hint: '{hint}'.")
+        for i in {0,2,4,6,8}:
+            if hint[i] == "-":
+                self._wrongLetters[int(i/2)].add(hint[i+1])
+            elif hint[i] == '+':
+                self._goodLetters[int(i/2)] = hint[i+1]
+                for c in allLetters:
+                    if c != hint[i+1]:
+                        self._wrongLetters[int(i/2)].add(c)
+            else:
+                self._wrongLetters[int(i/2)].add(hint[i+1])
+                self._neededLetters.add(hint[i+1])
+
 
      ###
      # Checks whether a given word matches all known hints
     def check(self, word):
         print(f"TODO: WordChecker testing if '{word}' verifies all known hints.")
+
+        for i in range(0,5):
+            if word[i] in self._wrongLetters[i]:
+                return False
+        
+        for c in self._neededLetters:
+            if c not in word:
+                return False
+
         return True
         
     ###
@@ -29,6 +56,6 @@ class WordChecker:
 if __name__ == "__main__":
     # creates a test WordChecker object and run through its methods
     wordChecker = WordChecker()
-    wordChecker.update("some hint")
-    print(wordChecker.check("some word"))
+    wordChecker.update("-H+O+U+S~E")
+    print(wordChecker.check("EOUST")) # COUST - False;  COUSE - False
     print(wordChecker)
